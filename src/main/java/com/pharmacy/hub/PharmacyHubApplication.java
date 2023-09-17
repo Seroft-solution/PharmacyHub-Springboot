e1ae09f49ac3647ee8a1f85b97d40df6ba341142
Syed Usman <syedus06@gmail.com>
2023-09-18 03:15:40 +0500

    Created inf

---
+++ src/main/java/com/pharmacy/hub/PharmacyHubApplication.java
@@ -0,0 +1,14 @@
+package com.pharmacy.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyHubApplication
+{
+  public static void main(String[] args)
+  {
+    SpringApplication.run(PharmacyHubApplication.class, args);
+  }
+
+}
