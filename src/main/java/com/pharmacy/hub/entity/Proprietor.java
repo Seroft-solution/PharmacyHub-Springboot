cbef432c5b0c7d066bae70138b08af89fec1c426
Syed Usman <syedus06@gmail.com>
2023-09-18 03:20:24 +0500

    Created entities

---
+++ src/main/java/com/pharmacy/hub/entity/Proprietor.java
@@ -0,0 +1,36 @@
+package com.pharmacy.hub.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "proprietor")
+public class Proprietor
+{
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String categoryRequired;
+  private String categoryDuration;
+  private String experienced;
+  private String pharmacyName;
+  private String city;
+  private String location;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "user_id", referencedColumnName = "id")
+  private User user;
+}
